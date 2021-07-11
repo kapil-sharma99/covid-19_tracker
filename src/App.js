bf1055c22e19da19f7a2c0ee8a33bea2eac8f00d
kapil-sharma99 <ks2822123@gmail.com>
2021-07-11 18:54:19 +0530

    UI started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import React from 'react';
+import {FormControl, Select, MenuItem, } from '@material-ui/core'
 
 function App() {
   return (
     <div className="app">
       <h1>Covid-19 Tracker App</h1>
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">WorldWide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Option 4</MenuItem>
+        </Select>
+      </FormControl>
+      {/* ---------- Header ---------- */}
+      {/* ----- Title + Select input dropdown field ----- */}
+
+      {/* ---------- Info boxes * 3 ---------- */}
+
+      {/* ---------- Table ---------- */}
+
+
+      {/* ---------- Graph ---------- */}
+
+      {/* ---------- Map ---------- */}
     </div>
   );
 }
