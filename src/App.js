b09324f0e7f20a3704a7f5723d25fc11bb915c17
kapil-sharma99 <ks2822123@gmail.com>
2021-07-11 19:59:03 +0530

    UI started and header created

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import {FormControl, Select, MenuItem, } from '@material-ui/core'
 function App() {
   return (
     <div className="app">
-      <h1>Covid-19 Tracker App</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">WorldWide</MenuItem>
-          <MenuItem value="worldwide">Option 2</MenuItem>
-          <MenuItem value="worldwide">Option 3</MenuItem>
-          <MenuItem value="worldwide">Option 4</MenuItem>
-        </Select>
-      </FormControl>
-      {/* ---------- Header ---------- */}
+			{/* ---------- Header ---------- */}
+			<div className="app__header">
+				<h1>Covid-19 Tracker</h1>
+				<FormControl className="app__dropdown">
+					<Select variant="outlined" value="abc">
+						<MenuItem value="worldwide">WorldWide</MenuItem>
+						<MenuItem value="worldwide">Option 2</MenuItem>
+						<MenuItem value="worldwide">Option 3</MenuItem>
+						<MenuItem value="worldwide">Option 4</MenuItem>
+					</Select>
+				</FormControl>
+			</div>
       {/* ----- Title + Select input dropdown field ----- */}
 
       {/* ---------- Info boxes * 3 ---------- */}
