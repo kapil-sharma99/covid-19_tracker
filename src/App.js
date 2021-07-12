3666972e6ec52b41fbf3ab69cdfcb5f7c6c88eee
kapil-sharma99 <ks2822123@gmail.com>
2021-07-12 19:02:38 +0530

    dropdown working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import {FormControl, Select, MenuItem, } from '@material-ui/core'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
-	const [countries, setCountries] = useState([]);
-	//Use effect runs a peice of code in given condition
-	useEffect(() => {
-		const getCountriesData = async () => {
-			await fetch("https://disease.sh/v3/covid-19/countries")
-			.then((response) => response.json())
-			.then((data) => {
-				const countries = data.map((country) => (
-					{
-						name: country.country, //United states of America, India, United Kingdom
-						value: country.countryInfo.iso2, //like IND, USA, UK
-					}
-				));
-				setCountries(countries);
-			});
-		};
-		getCountriesData();
-	}, []);
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  //Use effect runs a peice of code in given condition
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, //United states of America, India, United Kingdom
+            value: country.countryInfo.iso2, //like IND, USA, UK
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="app">
-			{/* ---------- Header ---------- */}
-			<div className="app__header">
-				<h1>Covid-19 Tracker</h1>
-				<FormControl className="app__dropdown">
-					<Select variant="outlined" value="abc">
-						{/* Loop through all the countires and show the drowdown */}
-						{countries.map(country => (
-							<MenuItem value={country}>{country}</MenuItem>
-						))}
-						{/* <MenuItem value="worldwide">WorldWide</MenuItem>
+      {/* ---------- Header ---------- */}
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country}>
+            {/* Loop through all the countires and show the drowdown */}
+            <MenuItem value="worldwide">WorldWide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            {/* <MenuItem value="worldwide">WorldWide</MenuItem>
 						<MenuItem value="worldwide">Option 2</MenuItem>
 						<MenuItem value="worldwide">Option 3</MenuItem>
 						<MenuItem value="worldwide">Option 4</MenuItem> */}
-					</Select>
-				</FormControl>
-			</div>
+          </Select>
+        </FormControl>
+      </div>
       {/* ----- Title + Select input dropdown field ----- */}
 
       {/* ---------- Info boxes * 3 ---------- */}
 
       {/* ---------- Table ---------- */}
-
 
       {/* ---------- Graph ---------- */}
 
