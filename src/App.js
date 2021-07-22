d005ed551be46f23ba73d2342d0618d179535de1
kapil-sharma99 <ks2822123@gmail.com>
2021-07-22 22:05:29 +0530

    Worldwide error resolved

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ function App() {
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
 
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  });
+
   //Use effect runs a peice of code in given condition
   useEffect(() => {
     const getCountriesData = async () => {
