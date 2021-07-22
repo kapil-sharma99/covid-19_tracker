bef2725bd49bbd69a7a41e2fb8a573b04a8fb58f
kapil-sharma99 <ks2822123@gmail.com>
2021-07-22 20:58:46 +0530

    Display Flex

--- src/App.js
+++ src/App.js
@@ -49,11 +49,11 @@ function App() {
       {/* ----- Title + Select input dropdown field ----- */}
 
       {/* ---------- Info boxes * 3 ---------- */}
-      <InfoBox title="Coronavirus Cases" total={2000} cases={123} />
-      <InfoBox title="Recovered" total={3000} cases={123} />
-      <InfoBox title="Deaths" total={4000} cases={123} />
-
-      <div className="app__stats"></div>
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" total={2000} cases={123} />
+        <InfoBox title="Recovered" total={3000} cases={123} />
+        <InfoBox title="Deaths" total={4000} cases={123} />
+      </div>
 
       {/* ---------- Table ---------- */}
 
