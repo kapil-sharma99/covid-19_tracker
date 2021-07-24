910968d979dd131613732ac0363151432a504c1a
kapil-sharma99 <ks2822123@gmail.com>
2021-07-24 11:31:05 +0530

    utility function created

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -3,13 +3,32 @@ import { Line } from "react-chartjs-2";
 
 function LineGraph() {
   const [data, setData] = useState({});
+
+  const buildChartData = (data, casesTypes = "cases") => {
+    const chartData = [];
+    let lastDataPoint;
+
+    data[casesTypes].forEach((date) => {
+      if (lastDataPoint) {
+        const newDataPoint = {
+          x: date,
+          y: data[casesTypes][date] - lastDataPoint,
+        };
+        chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[casesTypes][date];
+    });
+    return chartData;
+  };
+
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        const chartData = buildChartData(data);
       });
   });
+
   return <div>{/*<Line data options />*/}</div>;
 }
 
