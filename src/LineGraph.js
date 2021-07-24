02975e97666f10f50db50eb95d65f7e5587763c5
kapil-sharma99 <ks2822123@gmail.com>
2021-07-24 13:11:53 +0530

    function updated

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,5 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
+import { numeral, tooltips } from "numeral";
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    points: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltips.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxis: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(values).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
 
 function LineGraph() {
   const [data, setData] = useState({});
@@ -8,7 +53,7 @@ function LineGraph() {
     const chartData = [];
     let lastDataPoint;
 
-    data[casesTypes].forEach((date) => {
+    for (let date in data.cases) {
       if (lastDataPoint) {
         const newDataPoint = {
           x: date,
@@ -17,19 +62,38 @@ function LineGraph() {
         chartData.push(newDataPoint);
       }
       lastDataPoint = data[casesTypes][date];
-    });
+    }
     return chartData;
   };
 
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-      .then((response) => response.json())
-      .then((data) => {
-        const chartData = buildChartData(data);
-      });
-  });
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+        .then((response) => response.json())
+        .then((data) => {
+          const chartData = buildChartData(data);
+          setData = chartData;
+        });
+    };
+    fetchData();
+  }, []);
 
-  return <div>{/*<Line data options />*/}</div>;
+  return (
+    <div>
+      <Line
+        data={{
+          datasets: [
+            {
+              backgroundColor: "rgba(204, 16, 52, 0.5",
+              borderColor: "cc1034",
+              data: data,
+            },
+          ],
+        }}
+        options={options}
+      />
+    </div>
+  );
 }
 
 export default LineGraph;
